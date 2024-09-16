3495d5a280b31bf6a7bff25a3d3f1d0092b7d29e
imuridol <156184059+imuridol@users.noreply.github.com>
2024-09-17 02:05:05 +0500

    Finished

--- product/serializers.py
+++ product/serializers.py
@@ -17,18 +17,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    owner_full_name = serializers.SerializerMethodField()
-    category_name = serializers.SerializerMethodField()
+
 
     class Meta:
         model = ProductModel
-        fields = ('__all__')
-
-    def get_owner_full_name(self, obj):
-        return f'{obj.user.username}'
-
-    def get_category_name(self, obj):
-        return f'{obj.category.name}'
-
+        fields = ['id', 'name', 'price', 'owner', 'category']
 
 
--- product/views.py
+++ product/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
-from .models import *
+from rest_framework.filters import OrderingFilter
+from .serializers import *
 from rest_framework import generics
-from .serializers import ProductSerializer
-from rest_framework.viewsets import ModelViewSet
 from .filters import ProductFilter
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 # Create your views here.
 
-class ProductListview(ModelViewSet):
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = CategoryModel.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CategoryModel.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductListCreate(generics.ListCreateAPIView):
     queryset = ProductModel.objects.all()
     serializer_class = ProductSerializer
-    filterset_class = ProductFilter
-    filter_backends = DjangoFilterBackend
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filter_class = ProductFilter
+    ordering_fields = ['price']
+    ordering = ['price']
+
+class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductModel.objects.all()
+    serializer_class = ProductSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
